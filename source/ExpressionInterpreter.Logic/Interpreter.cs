2ca506c1c14cd0e3e813cd2f562fdd2ae43f7e01
Bernhard Grasch <bernhard.grasch@gmx.at>
2019-11-28 18:39:56 +0100

    Interpreter Properties implemented

--- source/ExpressionInterpreter.Logic/Interpreter.cs
+++ source/ExpressionInterpreter.Logic/Interpreter.cs
@@ -19,17 +19,17 @@ namespace ExpressionInterpreter.Logic
 
         public double OperandLeft
         {
-            get { throw new NotImplementedException(); }
+            get { return _operandLeft; }
         }
 
         public double OperandRight
         {
-            get { throw new NotImplementedException(); }
+            get { return _operandRight; }
         }
 
         public char Op
         {
-            get { throw new NotImplementedException(); }
+            get { return _op; }
         }
 
 
